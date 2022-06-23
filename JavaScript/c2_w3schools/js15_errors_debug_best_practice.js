81c586c9a9e006cf43e3c5ac588c5b46fc23bca2
martinez-ed <dev.martinezed@gmail.com>
2022-06-22 19:11:47 -0500

    JS Errors, Debugging and Best Practices

---
+++ JavaScript/c2_w3schools/js15_errors_debug_best_practice.js
@@ -0,0 +1,11 @@
+// Try and catch
+// Syntax: 
+// try {
+//     code to try
+// } catch (error) {
+//     code to handle the error
+// }
+// The try statement allows you to execute code that might throw an error.
+// The catch statement allows you to handle the error.
+// The try statement can have multiple catch statements.
+
